06f867bd3b4579ef223a05d753b328c8dcff8426
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2021-10-05 04:13:03 +0300

    Some improvements: change font in board, add info in main.
    Signed-off-by: iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>

--- app/src/main/java/ru/skeletikana/fastersudoku/MainActivity.java
+++ app/src/main/java/ru/skeletikana/fastersudoku/MainActivity.java
@@ -1,15 +1,10 @@
 package ru.skeletikana.fastersudoku;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
