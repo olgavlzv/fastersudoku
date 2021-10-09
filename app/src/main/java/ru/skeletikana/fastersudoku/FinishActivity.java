faa649f7bb971df614615c6cb4303268bde656d2
iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>
2021-10-09 08:24:33 +0300

    Best Score start
    Signed-off-by: iuriinesterov <55027018+iuriinesterov@users.noreply.github.com>

--- app/src/main/java/ru/skeletikana/fastersudoku/FinishActivity.java
+++ app/src/main/java/ru/skeletikana/fastersudoku/FinishActivity.java
@@ -2,15 +2,19 @@ package ru.skeletikana.fastersudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FinishActivity extends AppCompatActivity {
 
-    TextView txtScore;
+    TextView txtScore, txtBest;
     Button btnExitToMenu;
+    SharedPreferences preferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
 
     @Override
     public void onBackPressed() {
@@ -31,11 +35,31 @@ public class FinishActivity extends AppCompatActivity {
         txtScore = (TextView) findViewById(R.id.txtScore);
         txtScore.setText(fScore);
 
+        txtBest = (TextView) findViewById(R.id.txtBest);
+        String toBestScore = checkBest(fScore);
+        txtBest.setText(toBestScore);
+
         btnExitToMenu = (Button) findViewById(R.id.btnExitToMenu);
 
         btnExitToMenu.setOnClickListener(v -> {
             Intent intent = new Intent(FinishActivity.this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    public String checkBest(String fScore) {
+        int nowScore = Integer.parseInt(fScore);
+        int prevScore = preferences.getInt("bestScore", 0);
+
+        if (nowScore > prevScore) {
+            @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("bestScore", nowScore);
+            Toast.makeText(FinishActivity.this, "New best score!", Toast.LENGTH_SHORT).show();
+            prevScore = nowScore;
+        }
+
+        String setScore = String.valueOf(prevScore);
+
+        return ("Best score: " + setScore);
     }
 }
